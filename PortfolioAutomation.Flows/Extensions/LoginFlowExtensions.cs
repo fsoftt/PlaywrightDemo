dd814394fdde8efb2607ed94b583aa79a29c8fbb
Frank Silva Castro <frank_silva@epam.com>
2026-05-25 11:27:24 -0500

    add empty tests

---
+++ PortfolioAutomation.Flows/Extensions/LoginFlowExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+using PortfolioAutomation.Core.Extensions;
+using PortfolioAutomation.Pages.Pages;
+using PortfolioAutomation.TestData;
+
+namespace PortfolioAutomation.Flows.Extensions
+{
+    public static class LoginFlowExtensions
+    {
+        public static async Task<InventoryPage> Login(this IPage page)
+        {
+            await page.GoToHomePage();
+
+            var loginFlow = new LoginFlow(page);
+            return await loginFlow.LoginAsAsync(Auth.WorkingUser, Auth.Password);
+        }
+    }
+}
---
+++ PortfolioAutomation.Tests/InventoryTests.cs
@@ -0,0 +1,77 @@
+﻿using PortfolioAutomation.Core.Extensions;
+using PortfolioAutomation.Flows;
+using PortfolioAutomation.Flows.Extensions;
+using PortfolioAutomation.Pages.Pages;
+using PortfolioAutomation.TestData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioAutomation.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public class InventoryTests : TestBase
+    {
+        // TODO: move this to base class and make it overridable
+        [SetUp]
+        public async Task Setup()
+        {
+            context = await browser.NewContextAsync();
+            page = await context.NewPageAsync();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await ScreenshotIfFailed();
+            await context.CloseAsync();
+        }
+
+        public async Task Inventory_HasElements()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenOrderedAlphabetically_AreCorrectlyOrdered()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenOrderedAlphabeticallyDescending_AreCorrectlyOrdered()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenOrderedByPrice_AreCorrectlyOrdered()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenOrderedByPriceDescending_AreCorrectlyOrdered()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenImageClicked_IsRedirectedToItemPage()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenTitleClicked_IsRedirectedToItemPage()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenAddedOrRemovedFromCart_CartIsUpdated()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        public async Task Inventory_WhenCartIsViewed_ItemsAreDisplayed()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
